8346ec231452f41d0070142061cf55e3362343dc
Christopher Swinton <chris@theswintons.co.uk>
2020-10-20 14:21:00 +0100

    Amendemts to Part 3

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
+import { H1, MediumText } from "./HeroSection.tsx"
 
 function HeroSection() {
   return (
