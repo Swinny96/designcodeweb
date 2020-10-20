7c4c220aadec6d4381f8e2cd8860d8d8de208a75
Christopher Swinton <chris@theswintons.co.uk>
2020-10-20 14:08:04 +0100

    Part 3 Complete

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,15 +1,49 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 function HeroSection() {
   return (
-    <>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Design and code React Apps</h1>
-      <p>Don't skip design</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+            <Title>Design 
+            <br /> and code React apps
+            </Title>
+            <Description>
+            Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.
+            </Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 
 export default HeroSection
+
+const Wrapper = styled.div`
+    background: linear-gradient(180deg, #4316DB 0%, #9076E7 100%);
+`
+
+const ContentWrapper = styled.div`
+    max-width: 1234px;
+	padding: 200px 30px;
+	margin: 0 auto;
+`
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`
+
+const Description = styled.p `
+    font-size: 17px;
+	line-height: 130%;
+`
