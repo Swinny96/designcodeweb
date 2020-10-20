3946d80082d1d6086408bc23f683e803cc410fc0
Christopher Swinton <chris@theswintons.co.uk>
2020-10-20 15:25:05 +0100

    package updates for sharp, manifest and more

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,4 +17,18 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Available options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+        failOnError: true,
+      },
+    },
+  ]
 }
